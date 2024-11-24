e84ba6d490a51ce808cb4085158cc1e60ac19909
Amine Saoudi <saoudiamine8@gmail.com>
2024-11-24 02:47:32 -0500

    format better

--- popup.js
+++ popup.js
@@ -1,18 +1,16 @@
+import { generateFolderPath } from './pathToThisFile.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     const currentRootDirectoryElement = document.getElementById('currentRootDirectory');
-    const changeRootDirectoryButton = document.getElementById('changeRootDirectory');
 
     // Load and display the saved root directory
     chrome.storage.local.get(['rootDirectory'], (result) => {
         if (result.rootDirectory) {
             currentRootDirectoryElement.textContent = result.rootDirectory;
         } else {
-            currentRootDirectoryElement.textContent = 'Not set. Please configure in the options.';
+            currentRootDirectoryElement.textContent = (async () => {
+                generateFolderPath("HI")
+            });
         }
-    });
-
-    // Open the options page on button click
-    changeRootDirectoryButton.addEventListener('click', () => {
-        chrome.runtime.openOptionsPage();
     });
 });
